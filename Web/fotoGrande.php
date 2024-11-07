cb43dd1b36430feee0b67d368189b0eb233bf9a4
Mercedes <mercedeslibrero@gmail.com>
2024-11-07 11:34:16 +0100

    Web

---
+++ Web/fotoGrande.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<img src="images/ensaladas/<?php echo $_GET["botella"];?>" height="400px">
+</body>
+</html>
